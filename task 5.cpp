ade826bbdf81c7ea768c6deea948257ea5a7b5b0
MUSAIB BIN SAMEER <musaibmax64@gmail.com>
2026-03-07 15:15:58 +0530

    Update task 5.cpp

--- task 5.cpp
+++ task 5.cpp
@@ -2,58 +2,31 @@
 #include <string.h>
 
 int main() {
-    char str1[100], str2[100], result[200];
-    char choice;
+    char str1[100], str2[100], copy[100], rev[100];
     int i, len;
 
-    
-    printf("Enter the first string: ");
-    fgets(str1, sizeof(str1), stdin);
-    str1[strcspn(str1, "\n")] = '\0'; 
+    printf("Enter first string: ");
+    gets(str1);
 
-    printf("Enter the second string: ");
-    fgets(str2, sizeof(str2), stdin);
-    str2[strcspn(str2, "\n")] = '\0'; 
+    printf("Enter second string: ");
+    gets(str2);
 
-  
-    printf("\nChoose operation:\n");
-    printf("a) Addition (Concatenate strings)\n");
-    printf("b) Copy first string into result\n");
-    printf("c) Reverse first string\n");
-    printf("d) Length of first string\n");
-    printf("Enter choice (a/b/c/d): ");
-    scanf(" %c", &choice);
+    char add[200];
+    strcpy(add, str1);
+    strcat(add, str2);
+    printf("\nAddition (Concatenation): %s", add);
 
-    switch(choice) {
-        case 'a': 
-            strcpy(result, str1);  
-            strcat(result, str2);   
-            printf("Result after addition: %s\n", result);
-            break;
+    strcpy(copy, str1);
+    printf("\nCopied string: %s", copy);
 
-        case 'b':
-            strcpy(result, str1);
-            printf("Result after copying: %s\n", result);
-            break;
-
-        case 'c': 
-            len = strlen(str1);
-            for(i = 0; i < len; i++) {
-                result[i] = str1[len - 1 - i];
-            }
-            result[len] = '\0';
-            printf("Reversed string: %s\n", result);
-            break;
-
-        case 'd':
-            len = strlen(str1);
-            printf("Length of first string: %d\n", len);
-            break;
-
-        default:
-            printf("Invalid choice!\n");
+    len = strlen(str1);
+    for(i = 0; i < len; i++) {
+        rev[i] = str1[len - i - 1];
     }
+    rev[len] = '\0';
+    printf("\nReversed string: %s", rev);
+
+    printf("\nLength of first string: %lu", strlen(str1));
 
     return 0;
 }
-
