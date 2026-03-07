410064a218c11aacee54d77e1aa833f435d750bb
MUSAIB BIN SAMEER <musaibmax64@gmail.com>
2026-03-07 15:14:27 +0530

    Update task 1.cpp
    Refactored the program to remove vowels from a string using scanf instead of fgets and simplified the logic.

--- task 1.cpp
+++ task 1.cpp
@@ -1,27 +1,19 @@
 #include <stdio.h>
-#include <ctype.h>
-
-int isVowel(char ch) {
-    ch = tolower(ch);
-    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U');
-}
 
 int main() {
     char str[100];
-    int i, j = 0;
+    int i;
 
     printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
+    scanf("%s", str);
 
-    for (i = 0; str[i] != '\0'; i++) {
-        if (!isVowel(str[i])) {
-            str[j++] = str[i];
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'||
+           str[i]=='A'||str[i]=='E'||str[i]=='I'||str[i]=='O'||str[i]=='U') {
+            continue;
         }
+        printf("%c", str[i]);
     }
-    str[j] = '\0';
-
-    printf("String after removing vowels: %s", str);
 
     return 0;
 }
-
